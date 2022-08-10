e67416d80b32f2ee6d4f18d4c2b13010cd989917
Lollipop <96362005+TastyLollipop@users.noreply.github.com>
2022-08-10 17:12:51 +0200

    Add files via upload

---
+++ Source/Library/Threading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Threading
+    {
+        public void GenerateThread(int threadID)
+        {
+            switch (threadID)
+            {
+                case 0:
+                    Thread NetworkingThread = new Thread(new ThreadStart(Library.utils.FetchIndexesForDownload));
+                    NetworkingThread.IsBackground = true;
+                    NetworkingThread.Name = "Conversion Thread";
+                    NetworkingThread.Start();
+                    break;
+            }
+        }
+    }
+}
